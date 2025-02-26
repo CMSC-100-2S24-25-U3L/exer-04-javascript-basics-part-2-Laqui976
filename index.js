e21481b0cdad9e5d3756e39dacbd385f65d52e4c
Laqui976 <rplaqui@up.edu.ph>
2025-02-26 12:47:31 +0800

    feat:file save

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import validator from "validator";
 import { v4 as uuidv4 } from "uuid";
+import { openSync, closeSync, appendFileSync } from 'node:fs';
 
-function generateUniqueID(firstName, lastName) {
+
+export function generateUniqueID(firstName, lastName) {
   var fname = firstName.toLowerCase(); //get first letter of firstName
   var random = uuidv4().substring(0,8); //get random alphanumeric cut to 8 char
 
@@ -10,16 +12,25 @@ function generateUniqueID(firstName, lastName) {
 }
 
 
-console.log(generateUniqueID("Alan", "Turing"));
-
-function addAccount([firstName, lastName, email, age]){
-    if(firstName.length ===0 && lastName.length ===0 && email.length ===0 && age === null){
+export function addAccount([firstName, lastName, email, age]){
+    if(firstName.length ===0 && lastName.length ===0 && email.length ===0 && age === null){//cgeck if empty
         return "Cannot have an empty string";
     }else{
-        if(age>18 && validator.isEmail(email)){
-            return true;
+        if(age>18 && validator.isEmail(email)){//validate age and email
+            let fd;
+            var id = generateUniqueID(firstName, lastName);
+            const data = '${firstName}, ${lastName}, ${email}, ${age}, ${id}\n';
+                try {
+                fd = openSync('users.txt', 'a');
+                appendFileSync(fd, data , 'utf8');
+                } catch (err) {
+                    console.log(err);
+                } finally {
+                if (fd !== undefined)
+                    closeSync(fd);
+                } 
+                           
         }
     }
 }
 
-console.log(addAccount(['Alan', 'Turing', 'aturing@w3c.com', 58]));
---
+++ test.js
@@ -0,0 +1,4 @@
+import {generateUniqueID, addAccount} from "./index.js"
+
+console.log(generateUniqueID("Alan", "Turing"));
+addAccount(['Alan', 'Turing', 'aturing@w3c.com', 58]);
